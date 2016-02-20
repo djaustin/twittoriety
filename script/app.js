3c1aab149edc8827199d53b35bb0910fbc114054
Dan Austin <dja470@bham.ac.uk>
2016-02-20 23:56:46 +0000

    add function for levenshtein distance

--- script/app.js
+++ script/app.js
@@ -1,7 +1,11 @@
 angular.module('twittoriety', []).controller('InputController', ['$scope', function($scope){
   $scope.userInput = "";
   $scope.similarity = 0;
-  var sampleTweet = 'but if the self proclaimed cockiest person in the world can lay his personal business on the line then please people see my heart.';
+  $scope.compare = function(userTweet, sampleTweet){
+    var levenshtein = new Levenshtein(userTweet, sampleTweet);
+    $scope.similarity = levenshtein.distance
+  }
+  $scope.sampleTweet = 'but if the self proclaimed cockiest person in the world can lay his personal business on the line then please people see my heart.';
 
 
 
