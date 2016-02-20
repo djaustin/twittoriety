d1fde4d72b2d3133b729e5141c1197da9a637850
Dan Austin <dja470@bham.ac.uk>
2016-02-20 20:59:22 +0000

    added character counter

---
+++ script/app.js
@@ -0,0 +1,8 @@
+angular.module('twittoriety', []).controller('InputController', ['$scope', function($scope){
+  $scope.userInput = "";
+
+  var sampleTweet = {
+    author: "kanyewest",
+    content: "...on another note, can brah be the girl verson of bruh???"
+  };
+}]);
